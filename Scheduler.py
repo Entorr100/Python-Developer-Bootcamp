38b9d3f832550957854c9875658d3aef7e29fddb
Entorr100 <entorr100@yahoo.com>
2025-02-22 17:36:29 -0600

    finished to do list

--- Scheduler.py
+++ Scheduler.py
@@ -1,44 +1,79 @@
+import csv
+
+tasks = []
+with open("storage.csv", "r", newline="") as csvfile:
+    csv_reader = csv.DictReader(csvfile)
+    for row in csv_reader:
+        tasks.append(row)
+
+
 def print_to_do_list(to_do_list):
     for index, task in enumerate(to_do_list):
         if task["completed"] == "not completed":
-            print(f"{index}. {task["Name"]} at {task["scheduled time"]} is {task["completed"]}")
-        else: 
-            print(f"{index}. {task["Name"]} at {task["scheduled time"]} was {task["completed"]} at {task["completed time"]}")
-tasks = []
+            print(
+                f"{index}. {task["Name"]} at {task["scheduled time"]} is {task["completed"]}"
+            )
+        else:
+            print(
+                f"{index}. {task["Name"]} at {task["scheduled time"]} was {task["completed"]} at {task["completed time"]}"
+            )
+
+
 while True:
-        choice = input("view (v), Add Tasks (a), Quit (q), completed (c), remove task (r) > ")
-        if choice == "v":
-            print_to_do_list(tasks)
-        elif choice == "a":
-            addition = input("Please submit the task to add > ")
-            addition_time = input("Please submit the scheduled time > ")
-            tasks.append({"Name": addition, "completed": "not completed", "scheduled time": addition_time, "completed time": ""})
+    choice = input(
+        "view (v), Add Tasks (a), Quit (q), completed (c), remove task (r) > "
+    )
+    if choice == "v":
+        print_to_do_list(tasks)
+    elif choice == "a":
+        addition = input("Please submit the task to add > ")
+        addition_time = input("Please submit the scheduled time > ")
+        tasks.append(
+            {
+                "Name": addition,
+                "completed": "not completed",
+                "scheduled time": addition_time,
+                "completed time": "",
+            }
+        )
+        print("updated tasks")
+        print_to_do_list(tasks)
+    elif choice == "c":
+        print_to_do_list(tasks)
+        try:
+            task_index = int(input("Enter the task to complete > "))
+            tasks[task_index]["completed"] = "completed"
+            addition_time = input("Please submit the completed time > ")
+            tasks[task_index]["completed time"] = addition_time
             print("updated tasks")
             print_to_do_list(tasks)
-        elif choice == "c":
+        except ValueError:
+            print("Please enter a valid number!")
+        except IndexError:
+            print("That number is not on the list!")
+    elif choice == "r":
+        try:
             print_to_do_list(tasks)
-            try:
-                task_index = int(input("Enter the task to complete > "))
-                tasks[task_index]["completed"] = "completed"
-                addition_time = input("Please submit the completed time > ")
-                tasks[task_index]["completed time"] = addition_time
-                print("updated tasks")
-                print_to_do_list(tasks)
-            except ValueError:
-                print("Please enter a valid number!")
-            except IndexError:
-                print("That number is not on the list!")
-        elif choice == "r":
-            try:
-                print_to_do_list(tasks)
-                tasks.pop(int(input("Enter the task to remove > ")))
-                print_to_do_list(tasks)
-            except ValueError:
-                print("Please enter a valid number!")
-            except IndexError:
-                print("That number is not on the list!")
-        elif choice == "q":
-            break
-        else:
-            print("Invalid option")
+            tasks.pop(int(input("Enter the task to remove > ")))
+            print_to_do_list(tasks)
+        except ValueError:
+            print("Please enter a valid number!")
+        except IndexError:
+            print("That number is not on the list!")
+    elif choice == "q":
+        break
+    else:
+        print("Invalid option")
 print("Goodbye")
+with open("Storage.csv", "w", newline="") as csvfile:
+    fieldnames = [
+        "Name",
+        "completed",
+        "not completed",
+        "scheduled time",
+        "completed time",
+    ]
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for task in tasks:
+        writer.writerow(task)
